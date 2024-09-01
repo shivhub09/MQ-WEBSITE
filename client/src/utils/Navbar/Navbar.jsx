d81e33dd1c5a69d53c250ba65198584d42a1a88c
shivhub09 <shivnagori2020@gmail.com>
2024-09-02 01:20:42 +0530

    nav bar changes

--- client/src/utils/Navbar/Navbar.jsx
+++ client/src/utils/Navbar/Navbar.jsx
@@ -4,14 +4,20 @@ import './Navbar.css';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleDrawer = () => {
     setIsOpen(!isOpen);
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   const handleEnrollClick = () => {
     window.location.href = 'https://pay.marcosquay.com/form/index.php';
   };
+
   return (
     <div className="navbar-container">
       <div className="logo-container">
@@ -21,9 +27,18 @@ const Navbar = () => {
       <div className={`navbar-content ${isOpen ? 'open' : ''}`}>
         <div className="list-items">
           <ul className="navbar-list">
-            <li className="list-item"> <a href="/home">Home</a> </li>
-            <li className="list-item"><a href="/aboutUs">About Us</a> </li>
-            <li className="list-item"><a href="/ourOfferings">Our Offerings</a></li>
+            <li className="list-item"><a href="/home">Home</a></li>
+            <li className="list-item"><a href="/aboutUs">About Us</a></li>
+            <li className="list-item">
+  Our Offerings
+  <div className="dropdown-content">
+    <a href="/ourOfferings/in-school-program">In School Program</a>
+    <a href="/ourOfferings/sports-academy">Sports Academy</a>
+    <a href="/ourOfferings/ace">Ace</a>
+    <a href="/ourOfferings/brand-sponsorship">Brand and Sponsorship</a>
+  </div>
+</li>
+
             <li className="list-item"><a href="/gallery">Gallery</a></li>
             <li className="list-item"><a href="/career">Career</a></li>
             <li className="list-item"><a href="/contact">Contact</a></li>
