4914afa895eeeecf3506eb0217e6da0792d32d0e
shivhub09 <shivnagori2020@gmail.com>
2024-09-02 00:24:12 +0530

    about us completed

--- client/src/pages/AboutUsPage/AboutUsPage.jsx
+++ client/src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './AboutUsPage.css';
 import FounderImage from './Website About Us 7.png'
+import VisionImage from './Website About Us 3.png';
+import MissionImage from './Website About Us 5.png';
 import Footer from '../../utils/Footer/Footer';
 
 const AboutUsPage = () => {
@@ -61,30 +63,56 @@ const AboutUsPage = () => {
             </h3>
           </div>
         </section>
-        <section className='four'>
-    <div className="fourth-container">
-        <div className="ceo-message-container">
-            <div className="ceo-image">
-                <img src={FounderImage} alt="Founder Raahil Dhruva" />
+
+        <section className='third'>
+          <div className="vision-mission-container">
+            <div className="vision-container">
+              <img src={VisionImage} alt="" />
+
+              <div className="vision-container-content">
+                <h2>Vision</h2>
+                <p>Creating a robust culture of Health & Fitness in school through structured sports and physical education.</p>
+                <a href="">Get in Touch </a>
+              </div>
             </div>
-            <div className="ceo-message-content">
+            <div className="mission-container">
+
+
+              <div className="mission-container-content">
+                <h2>Mission</h2>
+                <p>To align and include all our stakeholders towards the common mission – Fit School.</p>
+                <a href="">Get in Touch </a>
+              </div>
+              <img src={MissionImage} alt="" />
+            </div>
+          </div>
+        </section>
+
+
+        <section className='four'>
+          <div className="fourth-container">
+            <div className="ceo-message-container">
+              <div className="ceo-image">
+                <img src={FounderImage} alt="Founder Raahil Dhruva" />
+              </div>
+              <div className="ceo-message-content">
                 <h3>OUR FOUNDER</h3>
                 <p>
-                    Raahil Dhruva, born in Mumbai, was an active child right from a young age. Playing at inter-school, inter-district, and inter-state competitions, Raahil was a champion across multiple sports like tennis, football, running, swimming, and more. Since moving to the UK at age 10, Raahil continued pursuing his passion for tennis and, with the help of excellent school programs, was able to thrive and better his game. To continue playing at a professional level, Raahil trained at the Sutton Tennis Academy to help achieve his goal of representing India at world tournaments.
+                  Raahil Dhruva, born in Mumbai, was an active child right from a young age. Playing at inter-school, inter-district, and inter-state competitions, Raahil was a champion across multiple sports like tennis, football, running, swimming, and more. Since moving to the UK at age 10, Raahil continued pursuing his passion for tennis and, with the help of excellent school programs, was able to thrive and better his game. To continue playing at a professional level, Raahil trained at the Sutton Tennis Academy to help achieve his goal of representing India at world tournaments.
                 </p>
                 <p>
-                    After multiple victories, Raahil wanted the same kind of opportunities available to more students in India. He went on to study BSc. Entrepreneurship with a minor in Corporate Communications and Finance, at Queens University – Charlotte, North Carolina, on a tennis scholarship, where he represented his university in tournaments across the USA, achieving a ranking of 12 in doubles in the NCAA tournaments.
+                  After multiple victories, Raahil wanted the same kind of opportunities available to more students in India. He went on to study BSc. Entrepreneurship with a minor in Corporate Communications and Finance, at Queens University – Charlotte, North Carolina, on a tennis scholarship, where he represented his university in tournaments across the USA, achieving a ranking of 12 in doubles in the NCAA tournaments.
                 </p>
                 <p>
-                    Raahil has always believed in a good balance between education and an active lifestyle, and he is working to instill this value in all the schools he collaborates with. MQ was started with his vision to make every student an active participant in their day, in society, and throughout their lives.
+                  Raahil has always believed in a good balance between education and an active lifestyle, and he is working to instill this value in all the schools he collaborates with. MQ was started with his vision to make every student an active participant in their day, in society, and throughout their lives.
                 </p>
                 <h2>Raahil Dhruva</h2>
                 <h3>Director | Marcos Quay</h3>
+              </div>
             </div>
-        </div>
-        <Footer />
-    </div>
-</section>
+            <Footer />
+          </div>
+        </section>
 
       </div>
 
