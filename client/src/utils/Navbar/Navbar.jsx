36a77995d6cf9f6e2ae05f9992321ef5c6fbba6b
shivhub09 <shivnagori2020@gmail.com>
2024-09-10 15:50:53 +0530

    navbar changes

--- client/src/utils/Navbar/Navbar.jsx
+++ client/src/utils/Navbar/Navbar.jsx
@@ -1,59 +1,59 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import logo from './marcos-quay-logo.png';
-import './Navbar.css';
+  import React, { useState } from 'react';
+  import { useNavigate } from 'react-router-dom';
+  import logo from './marcos-quay-logo.png';
+  import './Navbar.css';
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const navigate = useNavigate(); // Initialize the navigate hook
+  const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const navigate = useNavigate(); // Initialize the navigate hook
 
-  const toggleDrawer = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleDrawer = () => {
+      setIsOpen(!isOpen);
+    };
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+    const toggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+    };
 
-  const handleEnrollClick = () => {
-    window.location.href = 'https://pay.marcosquay.com/form/index.php';
-  };
+    const handleEnrollClick = () => {
+      window.location.href = 'https://pay.marcosquay.com/form/index.php';
+    };
 
-  return (
-    <div className="navbar-container">
-      <div className="logo-container">
-        <img src={logo} alt="Marcos Quay Logo" />
-      </div>
-
-      <div className={`navbar-content ${isOpen ? 'open' : ''}`}>
-        <div className="list-items">
-          <ul className="navbar-list">
-            <li className="list-item" onClick={() => navigate('/home')}>Home</li>
-            <li className="list-item" onClick={() => navigate('/aboutUs')}>About Us</li>
-            <li className="list-item">
-              Our Offerings
-              <div className="dropdown-content">
-                <div onClick={() => navigate('/ourOfferings/in-school-program')}>In School Program</div>
-                <div onClick={() => navigate('/ourOfferings/sports-academy')}>Sports Academy</div>
-                <div onClick={() => navigate('/ourOfferings/ace')}>Ace</div>
-                <div onClick={() => navigate('/ourOfferings/brand-sponsorship')}>Brand and Sponsorship</div>
-              </div>
-            </li>
-            <li className="list-item" onClick={() => navigate('/gallery')}>Gallery</li>
-            <li className="list-item" onClick={() => navigate('/career')}>Career</li>
-            <li className="list-item" onClick={() => navigate('/contact')}>Contact</li>
-          </ul>
+    return (
+      <div className="navbar-container">
+        <div className="logo-container">
+          <img src={logo} alt="Marcos Quay Logo" />
         </div>
 
-        <input type="button" className="enrolNowButton" value="ENROLL NOW" onClick={handleEnrollClick} />
+        <div className={`navbar-content ${isOpen ? 'open' : ''}`}>
+          <div className="list-items">
+            <ul className="navbar-list">
+              <li className="list-item" onClick={() => navigate('/home')}>Home</li>
+              <li className="list-item" onClick={() => navigate('/aboutUs')}>About Us</li>
+              <li className="list-item">
+                Our Offerings
+                <div className="dropdown-content">
+                  <div onClick={() => navigate('/ourOfferings/in-school-program')}>In School Program</div>
+                  <div onClick={() => navigate('/ourOfferings/sports-academy')}>Sports Academy</div>
+                  <div onClick={() => navigate('/ourOfferings/ace')}>Ace</div>
+                  <div onClick={() => navigate('/ourOfferings/brand-sponsorship')}>Brand and Sponsorship</div>
+                </div>
+              </li>
+              <li className="list-item" onClick={() => navigate('/gallery')}>Gallery</li>
+              <li className="list-item" onClick={() => navigate('/career')}>Career</li>
+              <li className="list-item" onClick={() => navigate('/contact')}>Contact</li>
+            </ul>
+          </div>
+
+          <input type="button" className="enrolNowButton" value="ENROLL NOW" onClick={handleEnrollClick} />
+        </div>
+
+        <button className="hamburger" onClick={toggleDrawer}>
+          {isOpen ? '✖' : '☰'}
+        </button>
       </div>
+    );
+  };
 
-      <button className="hamburger" onClick={toggleDrawer}>
-        {isOpen ? '✖' : '☰'}
-      </button>
-    </div>
-  );
-};
-
-export default Navbar;
+  export default Navbar;
