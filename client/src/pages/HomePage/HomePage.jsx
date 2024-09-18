83b579f830df184522971ec743983e91eff221e1
shivhub09 <shivnagori2020@gmail.com>
2024-09-18 13:12:41 +0530

    content change in what we do

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -53,15 +53,7 @@ const HomePage = () => {
           <div className="what-we-do-container-content-box">
             <div className="what-we-do-container-content">
               <p>
-                Welcome to Marcos Quay, where we empower the next generation through comprehensive sports education. Partnering with schools, we deliver expert-led programs that enhance physical skills, teamwork, and sportsmanship. Join us in fostering a healthier, more active future for our kids
-              </p>
-
-              <ul className="content-list-items">
-                <li><img src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png" alt="Icon" /> Less routine - more creativity</li>
-                <li><img src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png" alt="Icon" /> Hundreds of thousands saved</li>
-                <li><img src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png" alt="Icon" /> Scale Budget efficiently</li>
-              </ul>
-
+              Welcome to Marcos Quay, where we empower the next generation through comprehensive sports education. Partnering with schools, we deliver expert-led programs that enhance physical skills, teamwork, and sportsmanship. Join us in fostering a healthier, more active future for our kids.              </p>
               <input type="button" className='getInTouchBtn' value="Get In Touch   >" />
             </div>
             <div className="what-we-do-container-images">
