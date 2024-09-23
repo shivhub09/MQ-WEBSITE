6d7b92a91ecb86280ccb5976564cad420fcbb200
shivhub09 <shivnagori2020@gmail.com>
2024-09-24 00:33:09 +0530

    add the routing when clicking on the cards

--- client/src/pages/HomePage/components/utils/OurOfferingsCard.jsx
+++ client/src/pages/HomePage/components/utils/OurOfferingsCard.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import './OurOfferingsCard.css'
+import { useNavigate } from 'react-router-dom';
+import './OurOfferingsCard.css';
+
 const OurOfferingsCard = ({ img, title, content, url }) => {
+  const navigate = useNavigate();
+
+  function handleOnClick(url) {
+    navigate(`/ourOfferings/${url}`);
+  }
+
   return (
     <div className="ourOfferingsCard">
       <div className="ourOfferingsCard-image">
@@ -10,7 +18,12 @@ const OurOfferingsCard = ({ img, title, content, url }) => {
       <div className="ourOfferingsCard-content">
         <h3>{title}</h3>
         <p>{content}</p>
-        {/* <a href={url} className="ourOfferingsCard-link">Know More &#8250;</a> */}
+        <h4
+          className="ourOfferingsCard-link" 
+          onClick={() => handleOnClick(url)} // pass as a callback
+        >
+          Know More &#8250;
+        </h4>
       </div>
     </div>
   );
