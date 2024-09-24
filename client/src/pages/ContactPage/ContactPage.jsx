cfcf2a29649b4c74f60e27160f8e0affbdb52d60
ishashah2303 <ishashah2303@gmail.com>
2024-09-24 20:28:23 +0530

    contact page done

--- client/src/pages/ContactPage/ContactPage.jsx
+++ client/src/pages/ContactPage/ContactPage.jsx
@@ -48,13 +48,13 @@ const ContactPage = () => {
     <div className='container'>
       <section className='cpone'>
         <div className='cpcontainer'>
-          <div className='cpchild-container'>
+          <div className='cpchild-container' id="left">
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7539.869145525307!2d72.83053!3d19.110526!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c9c7ff71ffdb%3A0x696e976124578a30!2sMarcos%20Quay%20Sporting%20Excellence%20Pvt%20Ltd.!5e0!3m2!1sen!2sin!4v1725973618042!5m2!1sen!2sin" width="600" height="450" allowfullscreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
-          <div className='cpchild-container'>
+          <div className='cpchild-container' id="right">
             <div className='address'>
               <h1>Our address</h1>
-              <div className='content'>
+              <div className='content' id="add">
                 <div className='cpicon'>
                   <CiLocationOn size={25} color={"grey"}/>
                 </div>
@@ -71,26 +71,29 @@ const ContactPage = () => {
             </div>
             <div className='contact-us'>
               <h1>Our contacts</h1>
-              <div className='content' id="low">  
-                <div className='cpicon'>
-                  <IoCallOutline size={25} color={"grey"}/>
-                </div>
-                <div className='callus'>
-                  <p>Call us</p>
-                  <div className='phno'>
-                    <p><a href="tel:+917506843435">+91-7506843435</a></p>
-                    <p><a href="tel:+917738789829">+91-7738789829</a></p>
-                    
+              <div className='content'> 
+                <div className="callbox">
+                  <div className='cpicon'>
+                    <IoCallOutline size={25} color={"grey"}/>
+                  </div>
+                  <div className='callus'>
+                    <p>Call us</p>
+                    <div className='phno'>
+                      <p><a href="tel:+917506843435">+91-7506843435</a></p>
+                      <p><a href="tel:+917738789829">+91-7738789829</a></p>
+                    </div>
                   </div>
                 </div>
-                <div className='cpicon1'>
-                  <HiOutlineMailOpen size={25} color={"grey"}/>
-                </div>
-                <div className='emailus'>
-                  <p>Email Us</p>
-                  <div className='emailid'>
-                    <p><a href='mailto:info@marcosquay.com'>info@marcosquay.com</a></p>
-                    <p><a href='mailto:reena.sanghvi@marcosquay.com'>reena.sanghvi@marcosquay.com</a></p>
+                <div className="emailbox">
+                  <div className='cpicon1'>
+                    <HiOutlineMailOpen size={25} color={"grey"}/>
+                  </div>
+                  <div className='emailus'>
+                    <p>Email Us</p>
+                    <div className='emailid'>
+                      <p><a href='mailto:info@marcosquay.com'>info@marcosquay.com</a></p>
+                      <p><a href='mailto:reena.sanghvi@marcosquay.com'>reena.sanghvi@marcosquay.com</a></p>
+                    </div>
                   </div>
                 </div>
               </div>
