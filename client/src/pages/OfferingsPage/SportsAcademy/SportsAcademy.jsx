93c64a4843ea39da39f8440fd99b22b83e490a9a
shivhub09 <shivnagori2020@gmail.com>
2024-10-08 20:08:01 +0530

    reversing of  things

--- client/src/pages/OfferingsPage/SportsAcademy/SportsAcademy.jsx
+++ client/src/pages/OfferingsPage/SportsAcademy/SportsAcademy.jsx
@@ -1,4 +1,4 @@
-import {React, useEffect} from 'react';
+import { React, useEffect } from 'react';
 import './SportsAcademy.css';
 import SportsComplexImage from './Website Our Offerings - MQ Sports Academy.png';
 
